82fbb1db2f9aafb1aba2ad408b47db3a19497e19
Sylvester Keil <sylvester@keil.or.at>
2011-08-08 21:18:49 +0200

    added more delegators

--- lib/citeproc/processor.rb
+++ lib/citeproc/processor.rb
@@ -17,11 +17,12 @@ module CiteProc
 
     attr_reader :options, :engine, :items
     
-    def_delegators :@engine, :process, :append, :preview, :bibliography
+    def_delegators :@engine, :process, :append, :preview, :bibliography, :style, :style=, :abbreviate, :abbreviations, :abbreviations=
      
     def initialize(options = {})
       @options = Processor.defaults.merge(options)
-      @engine = Engine.default.new(:processor => self)
+      @engine = Engine.autodetect(@options).new(:processor => self)
+      @items = {}
     end
     
   end

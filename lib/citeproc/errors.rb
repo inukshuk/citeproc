7da70651c6677c19f9eb05949604deebeaa0edd5
Sylvester Keil <sylvester@keil.or.at>
2011-10-31 22:21:43 +0100

    use active exception as original by default

--- lib/citeproc/errors.rb
+++ lib/citeproc/errors.rb
@@ -3,11 +3,14 @@ module CiteProc
   class Error < StandardError
     attr_reader :original
 
-    def initialize(message, original = nil)
-      @original = original ? super([message, original.message].join(': ')) : super(message)
+    def initialize(message, original = $!)
+      @original = original
+			super(message)
     end    
   end
   
+	class ParseError < Error; end
+	
   class EngineError < Error; end
   
   class NotImplementedByEngine < EngineError; end

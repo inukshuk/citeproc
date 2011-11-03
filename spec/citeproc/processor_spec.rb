d703706d76952a95c329036975da999121d7e00c
Sylvester Keil <sylvester@keil.or.at>
2011-11-03 14:02:20 +0100

    added Processor#bibliography example descriptions

--- spec/citeproc/processor_spec.rb
+++ spec/citeproc/processor_spec.rb
@@ -8,5 +8,44 @@ module CiteProc
     
     it { should_not be nil }
     
+
+		describe '#bibliography (generates the bibliography)' do
+			
+			describe 'when no items have been processed' do
+				
+				it 'returns an empty bibliography'
+				
+				it 'returns a bibliography of all registered items if invoked with :all'
+				
+			end
+			
+			describe 'when items have been processed' do
+				
+				it 'returns a bibliography containing all cited items'
+
+				it 'returns a bibliography of all registered items if invoked with :all'
+				
+				describe 'when invoked with a block as filter' do
+				
+					it 'returns an empty bibliography if the block always returns false'
+					
+					it 'returns the full bibliography if the block always returns true'
+					
+					it 'returns a bibliography with all items for which the block returns true'
+					
+				end
+				
+				describe 'when passed a hash as argument' do
+				
+					it 'fails if the hash is no valid selector'
+					
+					it 'creates a selector from the hash and returns a bibliography containing all matching items'
+					
+				end
+				
+			end
+			
+		end
+		
   end
 end

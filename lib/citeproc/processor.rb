2b8ddc7cc8567d04eb20fc6af0be1c3fe5838ff7
Sylvester Keil <sylvester@keil.or.at>
2011-11-06 22:05:58 +0100

    fixed citation data creation

--- lib/citeproc/processor.rb
+++ lib/citeproc/processor.rb
@@ -103,11 +103,11 @@ module CiteProc
 
 
     def process(*arguments)
-      engine.process(CitationData.create(arguments.flatten(1)))
+      engine.process(CitationData.new(arguments.flatten(1)))
     end
 
     def append(*arguments)
-      engine.append(CitationData.create(arguments.flatten(1)))
+      engine.append(CitationData.new(arguments.flatten(1)))
     end
 
     def bibliography(*arguments, &block)

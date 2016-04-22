7c7d9a38c20c0cde4ba93ab690d99458410eea10
Sylvester Keil <sylvester@keil.or.at>
2016-04-22 11:07:33 +0200

    Whitespace

--- spec/citeproc/attributes_spec.rb
+++ spec/citeproc/attributes_spec.rb
@@ -2,52 +2,52 @@ require 'spec_helper'
 
 module CiteProc
   describe Attributes do
-  
+
     before(:all) do
-			A = Class.new { include Attributes }
-		end
-  
+      A = Class.new { include Attributes }
+    end
+
     let(:instance) do
-			o = A.new
-			o[:bar] = 'foo'
-			o
-		end
+      o = A.new
+      o[:bar] = 'foo'
+      o
+    end
 
     let(:other) do
-	 		o = A.new
-			o[:foo] = 'bar'
-			o
-		end
-  
+      o = A.new
+      o[:foo] = 'bar'
+      o
+    end
+
     it { is_expected.not_to be_nil }
-  
+
     describe '.attr_fields' do
-    
-			#       before(:all) do
-			# 	A.instance_eval { attr_fields :value, %w[ is-numeric punctuation-mode ] }
-			# end
+
+      #       before(:all) do
+      #   A.instance_eval { attr_fields :value, %w[ is-numeric punctuation-mode ] }
+      # end
 
       it 'generates setters for attr_field values' do
         # pending
         # lambda { A.new.is_numeric }.should_not raise_error
       end
-    
+
       it 'generates no other setters' do
         expect { A.new.some_other_value }.to raise_error
       end
     end
-  
-    describe '#merge' do    
-    
+
+    describe '#merge' do
+
       it 'merges non-existent values from other object' do
         expect(A.new.merge(other)[:foo]).to eq('bar')
       end
-    
+
       it 'does not overwrite existing values when merging other object' do
         expect(instance.merge(other)[:bar]).to eq('foo')
       end
-    
+
     end
-  
+
   end
 end

646e5a819ef8c85e735c49daa16d2a508145c404
Sylvester Keil <sylvester@keil.or.at>
2017-12-22 23:33:28 +0100

    Remove old Ruby upcase/downcase

--- lib/citeproc/compatibility.rb
+++ lib/citeproc/compatibility.rb
@@ -10,7 +10,6 @@ if RUBY_PLATFORM =~ /java/i
   puts java.lang.System.getProperty('file.encoding')
 
   module CiteProc
-
     def upcase(string)
       java.lang.String.new(string).to_upper_case(java.util.Locale::ENGLISH).to_s
     end
@@ -23,59 +22,12 @@ if RUBY_PLATFORM =~ /java/i
 else
 
   module CiteProc
-    if RUBY_VERSION >= '2.3'
-      def upcase(string)
-        string.upcase
-      end
+    def upcase(string)
+      string.upcase
+    end
 
-      def downcase(string)
-        string.downcase
-      end
-    else
-      begin
-        require 'unicode'
-
-        def upcase(string)
-          Unicode.upcase(string)
-        end
-
-        def downcase(string)
-          Unicode.downcase(string)
-        end
-      rescue LoadError
-        begin
-          require 'unicode_utils'
-
-          def upcase(string)
-            UnicodeUtils.upcase(string)
-          end
-
-          def downcase(string)
-            UnicodeUtils.downcase(string)
-          end
-        rescue
-          begin
-            require 'active_support/multibyte/chars'
-
-            def upcase(string)
-              ActiveSupport::Multibyte::Chars.new(string).upcase.to_s
-            end
-
-            def downcase(string)
-              ActiveSupport::Multibyte::Chars.new(string).downcase.to_s
-            end
-          rescue
-
-            def upcase(string)
-              string.upcase
-            end
-
-            def downcase(string)
-              string.downcase
-            end
-          end
-        end
-      end
+    def downcase(string)
+      string.downcase
     end
   end
 end
